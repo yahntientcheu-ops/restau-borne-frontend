150abe6f9398180aa7b854eb44370548f6a9077f
yahntientcheu-ops <yahntientcheu@gmail.com>
2026-01-06 10:08:19 +0100

    Update Categorie.java

--- src/main/java/fr/restau/model/Categorie.java
+++ src/main/java/fr/restau/model/Categorie.java
@@ -2,18 +2,18 @@ package fr.restau.model;
 
 public class Categorie {
     private String id;
-    private String nom;
+    private String date;
 
     public Categorie() {}
 
-    public Categorie(String id, String nom) {
+    public Categorie(String id, String date) {
         this.id = id;
-        this.nom = nom;
+        this.date = date;
     }
 
     // Getters et Setters
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
-    public String getNom() { return nom; }
-    public void setNom(String nom) { this.nom = nom; }
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
 }
